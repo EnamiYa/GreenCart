754555bf90a3437a78c6c59a914f46cadffe157f
Gigi <gigi_cheang@hotmail.com>
2023-02-19 04:44:33 -0500

    Code cleanup

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+var test_data = "Beef (beef herd)";
+
 var xhr = null;
 var data_result = null;
 
@@ -13,18 +15,13 @@ const getXmlHttpRequestObject = function () {
 
 function dataCallback() {
     if (xhr.readyState == 4 && xhr.status == 200) {
-        console.log("received response");
-        // dataDiv = document.getElementById('result-container');
-        // dataDiv.innerHTML = xhr.responseText;
-        // alert(xhr.responseText);
         data_result = JSON.parse(xhr.responseText);
         var div = document.getElementById('legend');
-        div.innerHTML = `Total emissions: ` + Math.round(data_result.Total_emissions*100)/100;
-        // alert(data_result.Total_emissions);
+        div.innerHTML = `Carbon emissions: ` + Math.round(data_result.Total_emissions*100)/100;
     }
 }
+
 function sendData(dat) {
-    // var input = document.getElementById('data-input').value;
     var data = {
         name: dat
     };
@@ -37,15 +34,11 @@ function sendData(dat) {
 
     xhr = getXmlHttpRequestObject();
     xhr.onreadystatechange = dataCallback;
-    // asynchronous requests
     xhr.open("PUT", "http://localhost:3000/get_stats", true);
     xhr.setRequestHeader("Content-Type", "application/json");
-    // Send the request over the network
-    console.log("sending: " + dataToSend);
     xhr.send(dataToSend);
 }
 
-//
 const getXmlHttpRequestObject2 = function () {
     if (!xhr2) {
         xhr2 = new XMLHttpRequest();
@@ -55,18 +48,12 @@ const getXmlHttpRequestObject2 = function () {
 
 function dataCallback2() {
     if (xhr2.readyState == 4 && xhr2.status == 200) {
-        console.log("received response");
-        // dataDiv = document.getElementById('result-container');
-        // dataDiv.innerHTML = xhr.responseText;
-        // alert(xhr.responseText);
         data_result2 = xhr2.responseText;
         var div = document.getElementById('openai-res');
-        div.innerHTML = data_result2.split("\n").join("<br />");
-        // alert(data_result2);
+        div.innerHTML = data_result2.split("\n").join("<br/>");
     }
 }
 function sendData2(dat) {
-    // var input = document.getElementById('data-input').value;
     var data = {
         name: dat
     };
@@ -79,56 +66,13 @@ function sendData2(dat) {
 
     xhr2 = getXmlHttpRequestObject2();
     xhr2.onreadystatechange = dataCallback2;
-    // asynchronous requests
     xhr2.open("POST", "https://gcheang.autocode.dev/ellehacks@0.0.2/", true);
     xhr2.setRequestHeader("Content-Type", "application/json");
-    // Send the request over the network
     console.log("sending: " + dataToSend);
     xhr2.send(dataToSend);
 }
 
-// const getXmlHttpRequestObject = function () {
-//     if (!xhr) {
-//         xhr = new XMLHttpRequest();
-//     }
-//     return xhr;
-// };
-
-// function dataCallback() {
-//     if (xhr.readyState == 4 && xhr.status == 200) {
-//         console.log("received response");
-//         // dataDiv = document.getElementById('result-container');
-//         // dataDiv.innerHTML = xhr.responseText;
-//         // alert(xhr.responseText);
-//         data_result = JSON.parse(xhr.responseText);
-//         var div = document.getElementById('legend');
-//         div.innerHTML = `Total emissions: ` + Math.round(data_result.Total_emissions*100)/100;
-//         alert(data_result.Total_emissions);
-//     }
-// }
-// function sendData(dat) {
-//     // var input = document.getElementById('data-input').value;
-//     var data = {
-//         name: dat
-//     };
-
-//     var dataToSend = JSON.stringify(data);
-//     if (!dat) {
-//         console.log("input was empty");
-//         return;
-//     }
-
-//     xhr = getXmlHttpRequestObject();
-//     xhr.onreadystatechange = dataCallback;
-//     // asynchronous requests
-//     xhr.open("PUT", "http://localhost:3000/get_stats", true);
-//     xhr.setRequestHeader("Content-Type", "application/json");
-//     // Send the request over the network
-//     console.log("sending: " + dataToSend);
-//     xhr.send(dataToSend);
-// }
-//
-async function update_title() {
+async function update_info() {
     var title;
     await chrome.tabs.query({active: true, currentWindow: true}, tabs => {
         var tab = tabs[0];
@@ -136,15 +80,15 @@ async function update_title() {
         console.log("Title: " + title);
         var element = document.getElementById("legendOne");
         element.innerHTML = title;
-        sendData("Bananas");
-        sendData2("Bananas");
+        sendData(test_data);
+        sendData2(test_data);
     });
 }
 
-addEventListener("load", (event) => { update_title(); });
+addEventListener("load", (event) => { update_info(); });
 
 chrome.tabs.onUpdated.addListener( async function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
-      update_title();
+      update_info();
     }
   });
